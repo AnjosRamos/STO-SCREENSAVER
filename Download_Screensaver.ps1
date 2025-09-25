525e64bf240837a440f983a01c2e01bc0dd8e9b6
AnjosRamos <phsbrasil@anjosramoscombr.onmicrosoft.com>
2025-09-25 13:40:35 -0300

    Add files via upload

--- Download_Screensaver.ps1
+++ Download_Screensaver.ps1
@@ -1,17 +1,17 @@
 # Bloco do Plano de Fundo 
-# Defina a URL do blob e o caminho de destino - Wallpaper
+# Defina a URL do blob e o caminho de destino - Screensaver
 $blobUrl1 = "https://raw.githubusercontent.com/AnjosRamos/STO-SCREENSAVER/main/Screensaver-institucional.jpg"
 $destinationPath1 = "C:\Screensaver\Screensaver-institucional.jpg"
 
-# Baixe a IMAGEM DO WALLPAPER do blob e salve no caminho de destino
+# Baixe a IMAGEM DO Screensaver do blob e salve no caminho de destino
 Invoke-WebRequest -Uri $blobUrl1 -OutFile $destinationPath1
 
 # Bloco da Tela de bloqueio - CASO NÃO PRECISE COMENTE AS LINHAS ABAIXO.
-# Defina a URL do blob e o caminho de destino - LockScreen
+# Defina a URL do blob e o caminho de destino - Screensaver
 $blobUrl2 = "https://raw.githubusercontent.com/AnjosRamos/STO-SCREENSAVER/main/Screensaver-sazonal.jpg"
 $destinationPath2 = "C:\Screensaver\Screensaver-sazonal.jpg"
 
-# Baixe a IMAGEM DO LOOK SCREEN do blob e salve no caminho de destino
+# Baixe a IMAGEM DO Screensaver do blob e salve no caminho de destino
 Invoke-WebRequest -Uri $blobUrl2 -OutFile $destinationPath2
 
 
